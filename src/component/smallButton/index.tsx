4eb5cfa98f379bf32e540ec1b56c9ca1fc5ef685
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2023-06-23 03:55:38 +0500

    add sass

--- src/component/smallButton/index.tsx
+++ src/component/smallButton/index.tsx
@@ -6,7 +6,7 @@ import { PlusIcon } from "../icon/Plus";
 import { MinusIcon } from "../icon/Minus";
 
 import styles from "./styles.module.css";
-import classnames from "classnames";
+import classnames from "classnames/bind";
 
 type ButtonProps = {
   isActive: boolean;
@@ -25,15 +25,18 @@ const getIcons = ({ iconType }: Pick<ButtonProps, "iconType">) => {
   }
 };
 
+let cx = classnames.bind(styles);
+
 export const SmallButton: FC<ButtonProps> = ({ isActive, color, iconType }) => {
   //TODO: add isActive class,color
   /*  const btnClass = classnames({ btn: true, btn__small: true }); */
 
   console.log("test");
 
-  return (
-    <button className={classnames(styles.button, styles.button_small)}>
-      {getIcons({ iconType })}
-    </button>
-  );
+  const classList = cx({
+    button: true,
+    button_small: true,
+  });
+
+  return <button className={classList}>{getIcons({ iconType })}</button>;
 };
