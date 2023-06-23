ed3f68569af607ec8b58c15f00f4207451a2d848
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2023-06-24 01:42:32 +0500

    style fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { MovieCard } from "@/component/movieCard";
 
 import { SmallButton } from "@/component/smallButton";
 
-const testList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const testList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
 export default function Page() {
   return (
